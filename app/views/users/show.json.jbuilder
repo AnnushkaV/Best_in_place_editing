0e8d3e9178103a1e17e9846f443b5c2721e288ff
AnnushkaV <verezub_anna@mail.ru>
2015-08-04 21:18:45 +0300

    first commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :gender, :public_profile, :bio, :created_at, :updated_at
---
+++ db/migrate/20150804172916_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :gender
+      t.boolean :public_profile
+      t.text :bio
+
+      t.timestamps null: false
+    end
+  end
+end
